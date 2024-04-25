4caa5c4658a49b6046b38cfc606873377c5537b0
Daniel Scott <danscottlearns@gmail.com>
2024-04-24 23:50:31 -0600

    seperate openai instance from buidtime

--- src/app/api/send-prompt/route.ts
+++ src/app/api/send-prompt/route.ts
@@ -1,27 +1,20 @@
-// app/api/send-prompt/route.ts
 import { NextResponse } from 'next/server';
 import OpenAI from 'openai';
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+import getOpenAIInstance from '../api';
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
     const prompt = body.prompt;
-    
+    const openai = getOpenAIInstance();
     const params: OpenAI.Chat.ChatCompletionCreateParams = {
       messages: [{ role: 'user', content: prompt }],
       model: 'gpt-3.5-turbo',
     };
-
     const chatCompletion: OpenAI.Chat.ChatCompletion = await openai.chat.completions.create(params);
-
     const response = {
       response: chatCompletion.choices[0].message?.content?.trim(),
     };
-
     return NextResponse.json(response);
   } catch (error) {
     console.error('Error:', error);
